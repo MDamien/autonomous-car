b70c24a22b805c1244a7ac184d4ac8646e4d84ac
Damien <damien@dam.io>
2015-04-01 12:00:11 +0200

    reorganize

--- car.py
+++ car.py
@@ -7,19 +7,41 @@ def take_input():
         if 'update' in line:
             break
         k,*v = line.strip().split()
-        v = list(map(float, v))
         if k == 'speedlimit':
+            v = list(map(float, v))
             v = {'curr_limit':v[0], 'dist_next_limit':v[1], 'next_limit':v[2]}
+        elif k == 'trafficlight':
+            if len(v) == 1:
+                continue
+            else:
+                v = {'distance':float(v[0]), 'state':v[1].lower(), 'remaining-time':float(v[2])}
         else:
+            v = list(map(float, v))
             v = v[0]
         S[k] = v
     return S
 
 def real_sim():
+    DATA = []
     while True:
         S = take_input()
         T,B = take_decision(S)
         send(T,B)
+
+        curr_limit_V = S['speedlimit']['curr_limit']
+        tl_time, tl_state,tl_dist = 0,"",0
+        if 'trafficlight' in S:
+            tl_time = S['trafficlight']['remaining-time']
+            tl_state = S['trafficlight']['state']
+            tl_dist = S['trafficlight']['distance']
+        DATA.append((S['time'], S['distance']/10, S['speed'],T/10-12, B/10-11, curr_limit_V,
+            tl_time, 60 if tl_state == 'green' else 50, tl_dist))
+
+        """        
+        if S['distance'] > 900:
+            plot(DATA)
+            plante
+        """
 
 def send(T,B):
     print("throttle %d" % T)
@@ -28,25 +50,25 @@ def send(T,B):
 def fake_sim():
     limits = [{
         'start':0,
-        'limit':130,
-    },{
-        'start':500,
         'limit':70,
-    },{
-        'start':700,
-        'limit':50,
-    },{
-        'start':1000,
-        'limit':100,
-    },{
-        'start':1450,
-        'limit':30,
     }]
+
+    lights = [{
+        'pos': 50,
+        't0':1,
+        'interval': 10,
+    },{
+        'pos': 150,
+        't0':17,
+        'interval': 13,
+    }]
+
     pos = 0.
     speed = 0.
     dt = 0.1
     steps = 0
     t = 0
+    energy_usage = 0
 
     DATA = []
 
@@ -62,32 +84,68 @@ def fake_sim():
             if limit['start'] > pos:
                 next_limit = limit
                 break
+
+        curr_light = None
+        for light in lights:
+            if light['pos'] > pos:
+                curr_light = light
+                break
+
         params = {
             'time': t,
             'distance': pos,
             'speed': speed,
         }
+        
         curr_limit_V = curr_limit['limit'] if curr_limit else 0
         next_limit_V = next_limit['limit'] if next_limit else 0
+        
         if curr_limit:
             params['speedlimit'] = {
                 'curr_limit': curr_limit['limit'],
                 'next_limit': next_limit['limit'] if next_limit else 0,
                 'dist_next_limit': next_limit['start']-pos if next_limit else 0,
             }
+        tl_time = 0
+        tl_state = 'sdq'
+        if curr_light:
+            tl_time = (curr_light['t0'] + t) % curr_light['interval']*2
+            if tl_time < curr_light['interval']:
+                tl_state = "green"
+            else:
+                tl_state = "red-or-something-similar"
+            params['trafficlight'] = {
+                'distance': curr_light['pos']-pos,
+                'state': tl_state,
+                'remaining-time': curr_light['interval'] - (tl_time % curr_light['interval'])
+            }
         T, B = take_decision(params)
-        #print('decision', T, B)
+        energy_usage += dt*(T/100*310+1)
+        print('decision', T, B)
+        prev_pos, prev_speed = pos, speed
         pos, speed = one_step(pos, speed, T,B,dt)
-        #print("results",pos, speed)
+        if curr_limit and speed > curr_limit_V+3:
+            print("!!SPEEDING!!", speed," > ", curr_limit_V)
+        if curr_light and prev_pos <= curr_light['pos'] <= pos:
+            if tl_state == "red":
+                print("!!RECKLESS-DRIVING!!", curr_light)
+            else:
+                print("CROSSED GREEN TRAFFIC LIGHT!")
 
         steps += 1
         t = steps*dt
 
-        DATA.append((t, speed,T/4+12, B/4+11, curr_limit_V))
+        if steps % 20 == 0:
+            print(t, pos, speed)
+        DATA.append((t, speed,T/10-12, B/10-11, curr_limit_V, energy_usage/100,
+            tl_time, 60 if tl_state == 'green' else 50))
+    
+    print("ENERGY",energy_usage)
     plot(DATA)
 
-def dico_search(S, sl):
-    bmin, bmax,T, B = -100, 100, 0, 0
+
+def dico_search(S, sl, tl):
+    bmin, bmax,T, B = -MAX_BRAKE_PRCT, 100, 0, 0
     while True:
         v = bmin+(bmax-bmin)//2
         if v == bmax-1 or v == bmin or abs(v) > 100:
@@ -97,22 +155,44 @@ def dico_search(S, sl):
             T = v
         else:
             B = -v
-        ok = do_and_emergency_break_all_the_way(S['distance'], S['speed'],T,B, sl)
+        ok,_ = do_and_emergency_break_all_the_way(S['distance'], S['speed'],T,B, sl, tl)
         if ok:
             bmin = v
         else:
             bmax = v
     return T,B
 
+def dico_search_tl(S, sl, tl):
+    bmin, bmax,T, B = 20, 100, 0, 0
+    while True:
+        v = bmin+(bmax-bmin)//2
+        if v == bmax-1 or v == bmin or abs(v) > 100:
+            break
+        T, B = 0, 0
+        if v > 0:
+            T = v
+        else:
+            B = -v
+        ok,pass_light = do_and_emergency_break_all_the_way(S['distance'], S['speed'],T,B, sl, tl)
+        if ok and pass_light:
+            bmin = v
+        else:
+            bmax = v
+
+    return ok, T,B
+
 def take_decision(S):
     T = 0
     B = 0
     sl = S.get('speedlimit')
+    tl = S.get('trafficlight')
 
     T = 100
     B = 0
     if sl:
-        T,B = dico_search(S, sl)
+        #ok, T,B = dico_search_tl(S, sl, tl)
+        #if not ok:
+        T,B = dico_search(S, sl, tl)
     return T,B
 
 import sys
--- mysim.py
+++ mysim.py
@@ -1,11 +1,16 @@
 MAX_FRICTION = 0.001 #km/h
-MAX_BRAKE = 3
+MAX_BRAKE = 19
+
+TL_SAFE_DISTANCE = 5
 
  #21 kmh toute les secondes ?
-MAX_THROTTLE = 12.1
+MAX_THROTTLE = 17
+
+MAX_BRAKE_PRCT = 100
+import sys
 
 def plot(data):
-    out = open('out.csv', 'w')
+    out = open('out/out.csv', 'w')
     for d in data:
         out.write(','.join(map(str,d))+'\n')
     import matplotlib.pyplot as plt
@@ -47,37 +52,55 @@ def one_step(p0, s0, T, B, dt):
     speed = max(0.0, speed)
     return pos, speed
 
-def do_and_emergency_break_all_the_way(p0, s0, T, B, sl):
-    if sl['curr_limit']-3 != 0:
-        if s0 > sl['curr_limit']:
+def do_and_emergency_break_all_the_way(p0, s0, T, B, sl, tl):
+    if sl['curr_limit'] != 0:
+        if s0-3 > sl['curr_limit']:
             return False
 
     def strategie(steps, pos, speed):
-        if steps < 10:
+        if steps < 3:
             return T, B
         else:
-            return 0,100
+            return 0,MAX_BRAKE_PRCT
 
-    #DATA = []
+    DATA = []
     next_limit_pos = p0+sl['dist_next_limit'] if sl['dist_next_limit'] else None
+    prev_pos = p0
+    result = True
     for t, pos, speed in simulate(strategie, p0, s0):
-        #DATA.append((t, pos,speed,sl['dist_next_limit'], next_limit_pos))
+        #check speeding
         if next_limit_pos and pos > next_limit_pos+10:
             break
         if sl:
             if sl['next_limit'] != 0:
-                if pos >= next_limit_pos:
+                if next_limit_pos and pos >= next_limit_pos:
                     if speed > sl['next_limit']:
-                        return False
+                        result =  False
+                        break
             if sl['curr_limit'] != 0:
                 if speed > sl['curr_limit']:
-                    return False
+                    result = False
+                    break
+
+        #check traffic light
+        if tl and prev_pos-TL_SAFE_DISTANCE <= p0+tl['distance'] <= pos+TL_SAFE_DISTANCE:
+            state_changed = tl['remaining-time'] - t < 0
+            green = tl['state'] == 'green'
+            if state_changed:
+                green != green
+            #DATA.append((t,pos/10, t, tl['distance']/10, tl['remaining-time'], 70 if green else 80))
+            if pos >= p0+tl['distance']-30 and not green:
+                result = False
+                break
+
+        prev_pos = pos
         if speed < 0.1:
             break
-    #if p0 > 2000000000000:
-    #    plot(DATA)
-    #    plante
-    return True
+    pass_next_light = False
+    if tl:
+        if pos > p0+tl['distance']:
+            pass_next_light = True
+    return result, pass_next_light
 
 if __name__ == "__main__":
     print("accelerate", do_and_emergency_break_all_the_way(50, \
